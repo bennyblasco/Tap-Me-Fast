fc19ad200c62e50ef5886408e2c052864f085bfe
Benjamin Inemugha <bennyblasco@gmail.com>
2020-07-15 23:56:14 +0100

    Setup

--- Tap Me Fast/ViewController.swift
+++ Tap Me Fast/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        textLabel.text = "Tap me fast"
+    }
+    
 }
 
